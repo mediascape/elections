cbe0ee750d33261e8b8c78bf7c9b55466d86f8c9
itamayo <itamayo@vicomtech.org>
2016-01-13 10:26:43 +0100

    Update mdadaptation.js
    Add more time for ensure first interval

--- www/js/mediascape/AdaptationToolkit/adaptation/multiDeviceAdaptation/mdadaptation.js
+++ www/js/mediascape/AdaptationToolkit/adaptation/multiDeviceAdaptation/mdadaptation.js
@@ -472,7 +472,7 @@ function($, applicationContext){
                                 console.log('^^^^^^^^^^^^^^^^^^^^^^^' + e.agentid + 'join===============');
                         }
                       }
-                },500);
+                },1000);
 
           }
       console.groupEnd();
